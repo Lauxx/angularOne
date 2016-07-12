5db9b0578e6f640849f8fca52c4dc40e43fd88a1
Lauxx <lm.nichols@hotmail.com>
2016-07-12 11:25:22 -0600

    linked into giphy api getting back data and plugging that into an image tag and running a function on a button click that invokes the api request to do a GET

--- mainControl.js
+++ mainControl.js
@@ -9,9 +9,14 @@ angular.module("firstApp").controller("mainControl", function($scope, mainServe)
 
 	// $scope.tools = [{ name: 'hammer', description: 'Pink with sparkles, hits nails'}, {name: 'spear', description: "Wooden and stabs things"}, {name: 'tomahawk', description: 'For bludgeon'}];
 
-
 	$scope.tools = mainServe.tools;
-
+	$scope.things;
+	$scope.getThings = function(){
+		mainServe.getServeThings().then(function(res){
+			$scope.things = res.data;
+			console.log($scope.things, 'I am control data');
+		});
+	};
 
 
 });
--- mainServe.js
+++ mainServe.js
@@ -1,6 +1,18 @@
 angular.module("firstApp").service('mainServe', function($http){
-//use of 'this' to reference any other function in our js files
+//use of 'this' to reference any other function/variables in our js files
+//api requests happen here
 
 	this.tools = [{ name: 'hammer', description: 'Pink with sparkles, hits nails'}, {name: 'spear', description: "Wooden and stabs things"}, {name: 'tomahawk', description: 'For bludgeon'}];
+
+	this.getServeThings = function(){
+		return $http({
+			method: 'GET', 
+			url: 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC'
+		}).then(function(res){
+			//linking to giphy api and able to get data
+			console.log(res.data, 'I am serve data');
+			return res.data
+		})
+	};
 
 });
